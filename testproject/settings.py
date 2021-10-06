6d7a499eb8dc70151a7d0c90bdd23b160b6d8623
Shubhi Agarwal <70809548+shubhiagarwal18@users.noreply.github.com>
2021-10-06 17:23:29 +0530

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -146,8 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
-EMAIL_HOST_USER='anubhav01498@gmail.com'
-EMAIL_HOST_PASSWORD='Anubhav@1234'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
 
 # EMAIL_HOST_USER='captainthcoc@gmail.com'
